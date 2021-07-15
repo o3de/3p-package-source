24113fa8d7e31a770111d308afc032cd7270ac45
lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>
2021-07-15 10:09:12 -0700

    Adds python, updates host list files to include it
    Synced the host list files from the internal repo, and also
    added the python package build scripts.
    
    Signed-off-by: lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>

---
+++ package-system/python/build_package_image.py
@@ -0,0 +1,43 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project. For complete copyright and license terms please see the LICENSE at the root of this distribution.
+# 
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+
+# this script builds python for linux and darwin_x64
+# and places the result in linux_x64/package or darwin_x64/package 
+import subprocess
+import sys
+import os
+import platform
+
+folder_names = { #   subfolder     interpreter     build script 
+    'darwin'     : ('darwin_x64' , 'Python.framework/Versions/3.7/bin/python3', 'make-python.sh'),
+    'linux'      : ('linux_x64'  , 'python/bin/python', 'make-python.sh'),
+    'windows'    : ('win_x64'    , 'python/python.exe', 'build_python.bat')
+}
+
+platformsys = platform.system().lower()
+
+# intentionally generate a keyerror if its not a good platform:
+subfolder_name, binary_relpath, build_script = folder_names[platformsys]
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+build_script_dir = os.path.join(script_dir, subfolder_name)
+test_script_name = os.path.join(script_dir, 'quick_validate_python.py')
+build_script_name = os.path.join(build_script_dir, build_script)
+
+# the built python is expected to be in build script dir/package/...
+python_dir = os.path.join(build_script_dir, 'package' )
+python_executable = os.path.join(python_dir, binary_relpath)
+
+# build python using the build script
+result_value = subprocess.run([build_script_name], shell=True, cwd=build_script_dir)
+
+if result_value.returncode != 0:
+    sys.exit(result_value.returncode)
+
+# test out the freshly created python executable:
+result_value = subprocess.run([python_executable, test_script_name], cwd=python_dir)
+sys.exit(result_value.returncode)
---
+++ package-system/python/win_x64/build_python.bat
@@ -0,0 +1,110 @@
+@setlocal enabledelayedexpansion
+@echo off
+
+REM
+REM Copyright (c) Contributors to the Open 3D Engine Project. For complete copyright and license terms please see the LICENSE at the root of this distribution.
+REM 
+REM SPDX-License-Identifier: Apache-2.0 OR MIT
+REM
+REM
+
+set ScriptDir=%~dp0
+set outputdir=%ScriptDir%package
+set tempdir=%ScriptDir%temp
+set python_src=%tempdir%\cpython
+
+rem dont allow python to read pip packages from user's local folder
+set PYTHONNOUSERSITE=1
+
+echo Building python from source - Basic requirements:
+echo     - Visual studio vc141 build tools installed (VS2017).  This can be installed into vs2019 or above.
+echo     - git installed
+echo. 
+echo  ... This will take about 10 minutes ...
+echo.
+
+set vswhere_location=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer
+
+echo adding %vswhere_location% to PATH
+set PATH=%vswhere_location%;%PATH%
+
+for /f "tokens=*" %%i in ('vswhere -property installationPath') do set VS2017_LOCATION=%%i
+
+echo Using Visual Studio: %VS2017_LOCATION%
+
+if NOT exist "%VS2017_LOCATION%\Common7\Tools\vsdevcmd.bat" (
+     echo Could not find visual studio 2017 installed
+    exit /B 1
+ )
+call "%VS2017_LOCATION%\Common7\Tools\vsdevcmd.bat"
+
+
+echo Clearing %tempdir% if present...
+rmdir /s /q %tempdir% > NUL
+echo Clearing %outputdir% if present...
+rmdir /s /q %outputdir% > NUL
+
+mkdir %outputdir%
+mkdir %tempdir%
+cd /d %tempdir%
+
+echo Cloning python from git using v3.7.10...
+git clone https://github.com/python/cpython.git --branch "v3.7.10" --depth 1
+if %ERRORLEVEL% NEQ 0 (
+    echo "Git clone failed"
+    exit /B 1
+)
+
+cd /d %python_src%
+call .\PCBuild\get_externals.bat
+
+msbuild.exe "%python_src%\PCbuild\pcbuild.proj" /t:Build /m /nologo /v:m /p:Configuration=Debug /p:Platform=x64 /p:IncludeExternals=true /p:IncludeSSL=true /p:IncludeTkinter=true /p:PlatformToolset=v141
+if %ERRORLEVEL% NEQ 0 (
+  echo Failed to build debug python
+  exit /B 1
+)
+echo building release...
+msbuild.exe "%python_src%\PCbuild\pcbuild.proj" /t:Build /m /nologo /v:m /p:Configuration=Release /p:Platform=x64 /p:IncludeExternals=true /p:IncludeSSL=true /p:IncludeTkinter=true /p:PlatformToolset=v141
+if %ERRORLEVEL% NEQ 0 (
+  echo Failed to build release python
+  exit /B 1
+)
+
+cd /d %python_src%
+echo installing PIP...
+.\PCBuild\amd64\Python.exe  -m ensurepip --upgrade
+if %ERRORLEVEL% NEQ 0 (
+  echo Failed to ensure pip is present.
+  exit /B 1
+)
+.\PCBuild\amd64\Python.exe -m pip install --upgrade pip
+
+echo creating the installation image...
+rem We'll actually use the real python dist builder to do this:
+cd /d %python_src%
+.\PCBuild\amd64\python.exe .\PC\layout\main.py --copy %outputdir%\python -v -d --include-stable --include-pip --include-distutils --include-tcltk --include-idle --include-tools --include-venv --include-dev --include-launchers
+if %ERRORLEVEL% NEQ 0 (
+  echo "Failed to call python's layout script (debug)"
+  exit /B 1
+)
+
+.\PCBuild\amd64\python.exe .\PC\layout\main.py --copy %outputdir%\python -v --include-stable --include-pip --include-distutils --include-tcltk --include-idle --include-tools --include-venv --include-dev --include-launchers
+if %ERRORLEVEL% NEQ 0 (
+  echo "Failed to call python's layout script (release)"
+  exit /B 1
+)
+
+echo copying package metadata and cmake files...
+rem But we do add our own few things...
+set ROBOCOPY_OPTIONS=/NJH /NJS /NP /NDL
+robocopy %ScriptDir% %outputdir% *.cmake PackageInfo.json %ROBOCOPY_OPTIONS%
+
+cd /d %ScriptDir%
+
+echo clearing temp dir...
+rmdir /s /q %tempdir%
+
+rem we leave only the output folder which is the actual output for packaging.
+echo this folder is ready for packaging: %outputdir%
+
+exit /B 0
