49eb937bd478bf58aa2886d014293ccd3d29a752
Steve Pham <spham@amazon.com>
2021-07-21 12:19:39 -0700

    Update windows build script to call test.and.py instead
    Signed-off-by: Steve Pham <spham@amazon.com>

--- package-system/azslc/build_azslc_windows.cmd
+++ package-system/azslc/build_azslc_windows.cmd
@@ -11,39 +11,15 @@ SET SRC_PATH=temp\src
 
 cd %SRC_PATH%
 
-REM call prepare_solution_win.bat continue
-
-REM IF %ERRORLEVEL% NEQ 0 (
-REM     ECHO "prepare_solution_win.bat Command Failed"
-REM     exit /b 1
-REM )
-
-call build_win.bat Debug
+REM Call the build and test script
+call python test.and.py
 
 IF %ERRORLEVEL% NEQ 0 (
-    ECHO "prepare_solution_win.bat Debug Command Failed"
-    exit /b 1
-)
-
-call build_win.bat Release
-
-IF %ERRORLEVEL% NEQ 0 (
-    ECHO "prepare_solution_win.bat Release Command Failed"
-    exit /b 1
-)
-
-cd tests
-
-call launch_tests.bat
-IF %ERRORLEVEL% NEQ 0 (
-    ECHO "Windows Tests Failed"
+    cd ..
     exit /b 1
 ) ELSE (
-    ECHO "Windows Tests Passed"
+    cd ..
+    exit /b 0
 )
-
-cd ..
-
-exit /b 0
 
 
