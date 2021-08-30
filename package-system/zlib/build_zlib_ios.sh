259916e6f3cb522600c106ce5207eb26018b7ecb
lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>
2021-08-30 10:31:44 -0700

    Updates the shell scripts to check for failure
    Signed-off-by: lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>

--- package-system/zlib/build_zlib_ios.sh
+++ package-system/zlib/build_zlib_ios.sh
@@ -10,4 +10,13 @@
 # makes "appplication" executables.  This is not great for making a sdk for ios.
 # this custom script just runs it building the static lib only:
 cmake -S temp/src -B temp/build -G Xcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_CXX_STANDARD=17 -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_C_FLAGS="-fPIC" -DSKIP_INSTALL_FILES=YES
+if [ $? -ne 0 ]; then
+    echo "Error generating build"
+    exit 1
+fi
+
 cmake --build temp/build --target zlibstatic --config Release -j 8
+if [ $? -ne 0 ]; then
+    echo "Error building"
+    exit 1
+fi
--- package-system/zlib/build_zlib_mac.sh
+++ package-system/zlib/build_zlib_mac.sh
@@ -7,4 +7,14 @@
 #
 
 cmake -S temp/src -B temp/build -G Xcode -DCMAKE_CXX_STANDARD=17 -DSKIP_INSTALL_FILES=YES
+if [ $? -ne 0 ]; then
+    echo "Error generating build"
+    exit 1
+fi
+
 cmake --build temp/build --target zlibstatic --config Release -j 8
+if [ $? -ne 0 ]; then
+    echo "Error building"
+    exit 1
+fi
+
