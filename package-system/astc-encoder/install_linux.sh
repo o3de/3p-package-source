27ec00af664f91387031e056fe931fa0d01f7290
Qing Tao <qingtao@amazon.com>
2021-09-15 01:18:38 -0700

    Update build package for linux
    Signed-off-by: Qing Tao <qingtao@amazon.com>

---
+++ package-system/astc-encoder/install_linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+BIN_PATH=$TARGET_INSTALL_ROOT/bin
+INCLUDE_PATH=$TARGET_INSTALL_ROOT/include
+
+mkdir -p $INCLUDE_PATH
+mkdir -p $BIN_PATH
+
+# copy LICENSE.txt and header file
+cp -f temp/src/LICENSE.txt $TARGET_INSTALL_ROOT/
+cp -f temp/src/Source/astcenc.h $INCLUDE_PATH/
+
+BUILD_PATH=temp/build/Source
+
+# copy static lib and executable
+cp -f $BUILD_PATH/astcenc-sse2 $BIN_PATH/
+cp -f $BUILD_PATH/libastcenc-sse2-static.a $BIN_PATH/
+
+exit 0
--- package-system/astc-encoder/install_mac.sh
+++ package-system/astc-encoder/install_mac.sh
@@ -6,18 +6,19 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
 BIN_PATH=$TARGET_INSTALL_ROOT/bin
-INCLUDE_PATH=%TARGET_INSTALL_ROOT/include
+INCLUDE_PATH=$TARGET_INSTALL_ROOT/include
 
 mkdir -p $INCLUDE_PATH
 mkdir -p $BIN_PATH
 
 # copy LICENSE.txt and header file
 cp -f temp/src/LICENSE.txt $TARGET_INSTALL_ROOT/
-cp -f temp/src/Source/astcenc.h $TARGET_INSTALL_ROOT/
+cp -f temp/src/Source/astcenc.h $INCLUDE_PATH/
 
 BUILD_PATH=temp/build/Source
 
 # copy static lib and executable
-cp -f $BUILD_PATH/Release/*.* $BIN_PATH/
+cp -f $BUILD_PATH/astcenc-neon $BIN_PATH/
+cp -f $BUILD_PATH/libastcenc-neon-static.a $BIN_PATH/
 
 exit 0
