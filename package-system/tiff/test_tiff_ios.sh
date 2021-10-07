12b7fb25218dc8eac14ba7c702830c3b4d3053ff
lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>
2021-10-07 12:40:07 -0700

    Updates the package builder to test on ios and mac
    Signed-off-by: lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>

--- package-system/tiff/test_tiff_ios.sh
+++ package-system/tiff/test_tiff_ios.sh
@@ -10,11 +10,12 @@ rm -rf temp/build_test
 mkdir temp/build_test
 
 cmake -S test -B temp/build_test -G Xcode \
-    -DCMAKE_TOOLCHAIN_FILE=../../../../Scripts/cmake/Platform/Mac/Toolchain_mac.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../../../../Scripts/cmake/Platform/iOS/Toolchain_ios.cmake \
     -DCMAKE_MODULE_PATH="$DOWNLOADED_PACKAGE_FOLDERS;$PACKAGE_ROOT" || exit 1
 
 cmake --build temp/build_test --parallel --config Release || exit 1
 
-temp/build_test/Release/test_TIFF.app/Contents/MacOS/test_TIFF || exit 1
+# we can't actually run it on ios, that'd require an emulator or device as well as
+# cert / signing - but we can at least make sure it compiles.
 
 exit 0
