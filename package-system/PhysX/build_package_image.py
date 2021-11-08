ccbe1bf1cd07bf48022c6269b2accc55ed5c61f6
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2021-11-08 15:36:08 -0500

    Fix typos.
    - List should be seperated by commas
    - 'PhysGpu' -> 'PhysXGpu'
    
    Signed-off-by: Yuriy Toporovskyy <toporovskyy.y@gmail.com>

--- package-system/PhysX/build_package_image.py
+++ package-system/PhysX/build_package_image.py
@@ -45,8 +45,8 @@ def main():
         },
         'windows': {
             'EXTRA_SHARED_LIBS': '\n'.join((
-                '${CMAKE_CURRENT_LIST_DIR}/PhysX/pxshared/$<IF:$<CONFIG:debug>,debug/,$<$<CONFIG:profile>:profile/>>bin/PhysXDevice64.dll'
-                '${CMAKE_CURRENT_LIST_DIR}/PhysX/pxshared/$<IF:$<CONFIG:debug>,debug/,$<$<CONFIG:profile>:profile/>>bin/PhysGpu_64.dll'
+                '${CMAKE_CURRENT_LIST_DIR}/PhysX/pxshared/$<IF:$<CONFIG:debug>,debug/,$<$<CONFIG:profile>:profile/>>bin/PhysXDevice64.dll',
+                '${CMAKE_CURRENT_LIST_DIR}/PhysX/pxshared/$<IF:$<CONFIG:debug>,debug/,$<$<CONFIG:profile>:profile/>>bin/PhysXGpu_64.dll'
             )),
             'EXTRA_STATIC_LIBS_NON_MONOLITHIC': '\n'.join((
                 '${PATH_TO_STATIC_LIBS}/LowLevel_static_64.lib',
