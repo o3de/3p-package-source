486283951a73d1f4e1d53cc82eeff6e592f8d0dc
lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>
2021-12-02 13:59:10 -0800

    Updates the mac version of ASTC Encoder to actually use release build
    Signed-off-by: lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>

--- package-system/astc-encoder/build_linux.sh
+++ package-system/astc-encoder/build_linux.sh
@@ -6,5 +6,9 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
 # Note: on x86/x64 platforms, O3DE requires a minimum of SSE 4.1, so we do request this.
-cmake -S temp/src -B temp/build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DISA_SSE41=ON || exit $?
+cmake -S temp/src -B temp/build -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DISA_SSE41=ON \
+    || exit $?
+
 cmake --build temp/build -j 8 || exit $?
