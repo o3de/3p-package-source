fe75f1a848baa88a2b9a616cce22e0bae13a066c
AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
2021-12-09 09:37:18 -0800

    First steps at adding assimp to the 3rd party packaging tool. Build step works for Windows, install and test steps need to be completed, and other platforms need to be handled.
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>

---
+++ package-system/assimp/build_assimp_windows.cmd
@@ -0,0 +1,33 @@
+@rem #
+@rem # Copyright (c) Contributors to the Open 3D Engine Project.
+@rem # For complete copyright and license terms please see the LICENSE at the root of this distribution.
+@rem # 
+@rem # SPDX-License-Identifier: Apache-2.0 OR MIT
+@rem #
+
+@rem # note that we explicitly turn off the compilation of all features that rely on 3rd Party Libraries
+@rem # except the ones we want.  This prevents the cmake build system from automatically finding things
+@rem # if they happen to be installed locally, which we don't want.
+
+@rem # cmake expects fowardslashes:
+set "DOWNLOADED_PACKAGE_FOLDERS=%DOWNLOADED_PACKAGE_FOLDERS:\=/%"
+
+cmake -S temp/src ^
+    -DBUILD_SHARED_LIBS=OFF ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_MODULE_PATH="%DOWNLOADED_PACKAGE_FOLDERS%" ^
+    -DASSIMP_BUILD_ZLIB=OFF ^
+    temp/src/CMakeLists.txt || exit /b 1
+cmake --build temp/src --config release || exit /b 1
+cmake --build temp/src --config debug || exit /b 1
+
+cmake -S temp/src ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_MODULE_PATH="%DOWNLOADED_PACKAGE_FOLDERS%" ^
+    -DASSIMP_BUILD_ZLIB=OFF ^
+    temp/src/CMakeLists.txt || exit /b 1
+cmake --build temp/src --config release || exit /b 1
+cmake --build temp/src --config debug || exit /b 1
+
+exit /b 0
