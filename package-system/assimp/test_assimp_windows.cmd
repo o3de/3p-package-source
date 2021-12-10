2cabba27d536cf7579e0901774f5e8d04ca8f7eb
AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
2021-12-10 13:16:32 -0800

    Added test step that changes to a safe directory for testing files to be created, runs the unit tests for assimp, and verifies they ran without error
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>

--- package-system/assimp/test_assimp_windows.cmd
+++ package-system/assimp/test_assimp_windows.cmd
@@ -4,3 +4,21 @@
 @rem # 
 @rem # SPDX-License-Identifier: Apache-2.0 OR MIT
 @rem #
+
+set curdir=%cd%
+
+rem The tests leave behind a lot of temp files in the current working directory,
+rem so change to a directory in temp to keep things clean
+cd temp
+mkdir test_out
+cd test_out
+
+%TARGET_INSTALL_ROOT%\bin\Debug\unit.exe || goto ExitWithError
+%TARGET_INSTALL_ROOT%\bin\Release\unit.exe || goto ExitWithError
+
+cd %curdir%
+exit /b 0
+
+:ExitWithError
+cd %curdir%
+exit /b 1
