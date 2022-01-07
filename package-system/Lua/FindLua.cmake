8ee93ba508378df874ce8d563515a7084c85d768
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2022-01-06 18:22:19 -0800

    Updates for Lua 3P Package
    * Fix add_library to use 'STATIC IMPORTED GLOBAL' instead of 'INTERFACE IMPORTED GLOBAL'
    * Added 'IMPORT_LOCATION' to the target properties
    * Added link library dependency on ${CMAKE_DL_LIBS} (defined only on Linux) to fix linker error on gcc
    * Removed '10767{10767{MY_NAME}_LIBRARY} ' from target_link_libraries
    * Updates to config and FindLua.cmake to only use the release build of the lib for all platforms
    * Bump the revision number for all platforms to 7
    * Added OSX_DEPLOYMENY_TARGET for iOS
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

---
+++ package-system/Lua/FindLua.cmake
@@ -0,0 +1,31 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+# 
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+
+# this file actually ingests the library and defines targets.
+set(TARGET_WITH_NAMESPACE "3rdParty::Lua")
+if (TARGET ${TARGET_WITH_NAMESPACE})
+    return()
+endif()
+
+set(MY_NAME "Lua")
+
+set(${MY_NAME}_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/Lua/include)
+set(${MY_NAME}_LIBS_DIR ${CMAKE_CURRENT_LIST_DIR}/Lua/lib)
+set(${MY_NAME}_LIBRARY ${${MY_NAME}_LIBS_DIR}/release/${CMAKE_STATIC_LIBRARY_PREFIX}lualib${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+add_library(${TARGET_WITH_NAMESPACE} STATIC IMPORTED GLOBAL)
+
+set_target_properties(${TARGET_WITH_NAMESPACE} PROPERTIES IMPORTED_LOCATION "${${MY_NAME}_LIBRARY}")
+
+ly_target_include_system_directories(TARGET ${TARGET_WITH_NAMESPACE} INTERFACE ${${MY_NAME}_INCLUDE_DIR})
+
+target_link_libraries(${TARGET_WITH_NAMESPACE} 
+    INTERFACE ${CMAKE_DL_LIBS}
+)
+
+set(${MY_NAME}_FOUND True)
