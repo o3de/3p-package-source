d10b1c0f8eafa9c0c7a3eb240587a3649ebf1e2b
Vincent Liu <5900509+onecent1101@users.noreply.github.com>
2022-01-11 17:26:28 -0800

    [LYN-4620] Upgrade Mac AWSNativeSDK to 1.9.50 (#47)
    * [LYN-4620] Upgrade Mac AWSNativeSDK to 1.9.50
    * Add min osx version to get rid off linking warning
    * Cleanup find cmake

--- package-system/AWSNativeSDK/build_AWSNativeSDK_mac.sh
+++ package-system/AWSNativeSDK/build_AWSNativeSDK_mac.sh
@@ -8,6 +8,10 @@
 
 src_path=temp/src
 bld_path=temp/build
+inst_path=temp/install
+
+echo "Command: rm -rf $inst_path"
+rm -rf $inst_path || (echo "Command: rm -rf $inst_path failed" ; exit 1)
 
 configure_and_build() {
     build_type=$1
@@ -19,19 +23,21 @@ configure_and_build() {
     fi
 
     echo "CMake Configure $build_type $lib_type"
-    CXXFLAGS=-Wno-deprecated-declarations cmake -S "$src_path" -B "$bld_path/${build_type}_${lib_type}" \
+    CFLAGS="-Wno-deprecated-declarations -fPIC" CXXFLAGS="-Wno-deprecated-declarations -fPIC" cmake -S "$src_path" -B "$bld_path/${build_type}_${lib_type}" \
           -G "Xcode" \
           -DTARGET_ARCH=APPLE \
           -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+          -DCMAKE_OSX_DEPLOYMENT_TARGET="11.0" \
           -DCMAKE_CXX_STANDARD=17 \
+          -DCPP_STANDARD=17 \
           -DENABLE_TESTING=OFF \
           -DENABLE_RTTI=ON \
           -DCUSTOM_MEMORY_MANAGEMENT=ON \
           -DBUILD_ONLY="access-management;cognito-identity;cognito-idp;core;devicefarm;dynamodb;gamelift;identity-management;kinesis;lambda;mobileanalytics;queues;s3;sns;sqs;sts;transfer" \
           -DBUILD_SHARED_LIBS=$build_shared \
           -DCMAKE_BUILD_TYPE=$build_type \
-          -DCMAKE_INSTALL_BINDIR="bin/$build_type" \
-          -DCMAKE_INSTALL_LIBDIR="lib/$build_type" || (echo "CMake Configure $build_type $lib_type failed" ; exit 1)
+          -DCMAKE_INSTALL_BINDIR="bin" \
+          -DCMAKE_INSTALL_LIBDIR="lib" || (echo "CMake Configure $build_type $lib_type failed" ; exit 1)
 
     echo "CMake Build $build_type $lib_type to $bld_path/${build_type}_${lib_type}"
     cmake --build "$bld_path/${build_type}_${lib_type}" --config $build_type -j 12 || (echo "CMake Build $build_type $lib_type to $bld_path/${build_type}_${lib_type} failed" ; exit 1)
