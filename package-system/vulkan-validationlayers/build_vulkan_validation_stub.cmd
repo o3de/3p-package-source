a8c093f498e61e9ff7a39554d547c147d6632d18
Peng <tonypeng@amazon.com>
2022-01-26 15:50:27 -0800

    Add cmake stubs for Linux and Android
    Signed-off-by: Peng <tonypeng@amazon.com>

---
+++ package-system/vulkan-validationlayers/build_vulkan_validation_stub.cmd
@@ -0,0 +1,8 @@
+@rem #
+@rem # Copyright (c) Contributors to the Open 3D Engine Project.
+@rem # For complete copyright and license terms please see the LICENSE at the root of this distribution.
+@rem # 
+@rem # SPDX-License-Identifier: Apache-2.0 OR MIT
+@rem #
+
+echo Building stub for Android that is not supported
