d3ece44abfb57ab3df8c657ea8c5917299ee964e
moudgils <47460854+moudgils@users.noreply.github.com>
2022-02-10 12:41:42 -0800

    Addressed feedback
    Signed-off-by: moudgils <47460854+moudgils@users.noreply.github.com>

--- package-system/vulkan-validationlayers/install_vulkan_validation_windows.cmd
+++ package-system/vulkan-validationlayers/install_vulkan_validation_windows.cmd
@@ -1,10 +1,10 @@
-@rem #
-@rem # Copyright (c) Contributors to the Open 3D Engine Project.
-@rem # For complete copyright and license terms please see the LICENSE at the root of this distribution.
-@rem # 
-@rem # SPDX-License-Identifier: Apache-2.0 OR MIT
-@rem #
-@rem #
+@rem
+@rem Copyright (c) Contributors to the Open 3D Engine Project.
+@rem For complete copyright and license terms please see the LICENSE at the root of this distribution.
+@rem 
+@rem SPDX-License-Identifier: Apache-2.0 OR MIT
+@rem
+@rem
 @setlocal
 
 set OUT_PATH=%TARGET_INSTALL_ROOT%
@@ -18,7 +18,14 @@ mkdir %OUT_PATH%\\lib\\release
 mkdir %OUT_PATH%\\lib\\debug
 
 copy %BLD_PATH%\\install\\lib\\release\\* %OUT_PATH%\\lib\\release\\
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Unable to copy to %BLD_PATH%\\install\\lib\\release\\
+    exit /b 1
+)
 copy %BLD_PATH%\\install\\lib\\debug\\* %OUT_PATH%\\lib\\debug\\
-@rem @if %errorlevel% NEQ 0 ( exit /b 1 )
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Unable to copy to %BLD_PATH%\\install\\lib\\debug\\
+    exit /b 1
+)
 
-@rem exit /b 0
+exit /b 0
