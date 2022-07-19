7402c35ba618f28a9dd582a9e7c26e88c37b0818
spham-amzn <82231385+spham-amzn@users.noreply.github.com>
2022-07-19 15:10:01 -0700

    Updates for python 3.10.5 for Linux
    Signed-off-by: spham-amzn <82231385+spham-amzn@users.noreply.github.com>

--- package-system/python/linux_x64/make-python.sh
+++ package-system/python/linux_x64/make-python.sh
@@ -47,10 +47,10 @@ mkdir -p temp
 
 
 echo ""
-echo "--------------- Cloning python 3.7.12 from git ---------------"
+echo "--------------- Cloning python 3.10.5 from git ---------------"
 echo ""
 cd temp
-git clone https://github.com/python/cpython.git --branch v3.7.12 --depth 1
+git clone https://github.com/python/cpython.git --branch v3.10.5 --depth 1
 
 if [[ ! -d "cpython" ]]; then
     echo "Was unable to create cpython dir via git clone.  Is git installed?"
@@ -72,32 +72,7 @@ cp -f -v libexpat/expat/lib/*.c cpython/Modules/expat/
 
 
 echo ""
-echo "--------------- Cloning bzip2 1.0.8 and building ---------------"
-echo ""
-git clone git://sourceware.org/git/bzip2.git --branch "bzip2-1.0.8" --depth 1
-if [[ ! -d "bzip2" ]]; then
-    echo "Was unable to create bzip2 dir via git clone.  Is git installed?"
-    exit 1
-fi
-
-pushd bzip2
-
-PATCH_FILE=$SCRIPT_DIR/open3d_bzip2.patch
-echo Applying patch file $PATCH_FILE
-git apply --ignore-whitespace $PATCH_FILE
-if [ $retVal -ne 0 ]; then
-    echo "Git apply failed"
-    exit $retVal
-fi
-
-make bzip2
-
-make install PREFIX=install
-
-popd
-
-echo ""
-echo "--------------- Cloning libffi 1.0.8 and building static version ---------------"
+echo "--------------- Cloning libffi 3.4.2 and building static version ---------------"
 echo ""
 git clone https://github.com/libffi/libffi.git --branch "v3.4.2" --depth 1
 if [[ ! -d "libffi" ]]; then
@@ -115,7 +90,7 @@ if [ $retVal -ne 0 ]; then
     exit $retVal
 fi
  
-./configure --prefix=$SCRIPT_DIR/temp/ffi_lib --enable-shared=no --with-fpic=yes CFLAGS='-fPIC' CPPFLAGS='-fPIC'
+./configure --prefix=$SCRIPT_DIR/temp/ffi_lib --enable-shared=no CFLAGS='-fPIC' CPPFLAGS='-fPIC'
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "Error running configuring for libffi"
@@ -139,7 +114,7 @@ echo "--------------- Building cpython from source ---------------"
 echo ""
 
 # Build from the source with optimizations and shared libs enabled , and override the RPATH and bzip include/lib paths
-./configure --prefix=$SCRIPT_DIR/package/python --enable-optimizations --enable-shared LDFLAGS='-Wl,-rpath=\$$ORIGIN:\$$ORIGIN/../lib:\$$ORIGIN/../.. -L../bzip2/install/lib -L../ffi_lib/lib' CPPFLAGS='-I../bzip2/install/include -I../ffi_lib/include' CFLAGS='-I../temp/bzip2/install/include -I../ffi_lib/include'
+./configure --prefix=$SCRIPT_DIR/package/python --enable-optimizations --enable-shared LDFLAGS='-Wl,-rpath=\$$ORIGIN:\$$ORIGIN/../lib:\$$ORIGIN/../.. -L../ffi_lib/lib' CPPFLAGS='-I../ffi_lib/include' CFLAGS='-I../ffi_lib/include'
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "Error running configuring optimized build"
@@ -156,7 +131,7 @@ fi
 # Prepare the package folder
 cd $SCRIPT_DIR
 
-# Install the newly built python 3.7.12 to the package/python folder
+# Install the newly built python 3.10.5 to the package/python folder
 cd $SCRIPT_DIR
 cd temp
 cd cpython
@@ -211,10 +186,10 @@ rm -v $SCRIPT_DIR/package/python/lib/python3.7/distutils/command/wininst-*.exe
 echo "Removing out of date pip*.whl"
 rm -v $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/_bundled/pip-*.whl
 
-echo "Removing pip references from ensurepip"
-cat $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py | sed 's/"20.1.1"/"22.0.3"/g' | sed 's/("pip", _PIP_VERSION, "py2.py3"),//g' > $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py_temp
-rm $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py
-mv $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py_temp $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py
+# echo "Removing pip references from ensurepip"
+#    cat $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py | sed 's/"20.1.1"/"22.0.3"/g' | sed 's/("pip", _PIP_VERSION, "py2.py3"),//g' > $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py_temp
+#rm $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py
+#mv $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py_temp $SCRIPT_DIR/package/python/lib/python3.7/ensurepip/__init__.py
 
 echo ""
 echo "--------------- PYTHON WAS BUILT FROM SOURCE ---------------"
