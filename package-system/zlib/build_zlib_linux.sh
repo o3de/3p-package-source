046fa7eaf1753842cb80006b005b173f536739a6
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2022-11-15 22:44:47 +0000

    Script updates for zlib on the linux aarch64 platform
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- package-system/zlib/build_zlib_linux.sh
+++ package-system/zlib/build_zlib_linux.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #
 # Copyright (c) Contributors to the Open 3D Engine Project.
 # For complete copyright and license terms please see the LICENSE at the root of this distribution.
@@ -5,6 +7,21 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 #
 #
+
+
+# Building zlib on ubuntu requires the following packages
+REQUIRED_DEV_PACKAGES="cmake ninja-build gcc"
+ALL_PACKAGES=`apt list | grep installed  2>/dev/null`
+for req_package in $REQUIRED_DEV_PACKAGES
+do
+    PACKAGE_COUNT=`echo $ALL_PACKAGES | grep $req_package | wc -l`
+    if [[ $PACKAGE_COUNT -eq 0 ]]; then
+        echo "Missing required package '${req_package}'. Install this package and try again."
+        exit 1
+    fi
+done
+
+
 
 cmake -S temp/src -B temp/build \
      -G Ninja \
