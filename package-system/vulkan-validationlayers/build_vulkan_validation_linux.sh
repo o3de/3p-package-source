7b7c7dcfa7ccfd4513eb3be4e4b65dd325fb40f8
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-02-24 16:33:54 -0800

    Dockerize the linux build for vulkan validation layers and support arm64 builds (#182)
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- package-system/vulkan-validationlayers/build_vulkan_validation_linux.sh
+++ package-system/vulkan-validationlayers/build_vulkan_validation_linux.sh
@@ -5,15 +5,38 @@
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
-
+TEMP_FOLDER=/data/workspace
 SRC_DIR=$TEMP_FOLDER/src
 BUILD_DIR=$TEMP_FOLDER/build
 TMP_RELEASE_DIR=$BUILD_DIR/install/lib/release
 
-$PYTHON_BINARY $SRC_DIR/scripts/update_deps.py --dir $SRC_DIR/external --arch x64 --config release
-cmake -G "Ninja Multi-Config" -C $SRC_DIR/external/helper.cmake -S $SRC_DIR -B $BUILD_DIR
+python3 $SRC_DIR/scripts/update_deps.py --dir $TEMP_FOLDER/external --arch x64 --config release
+if [ $? -ne 0 ]
+then
+    echo "Error configuring build environment"
+    exit 1
+fi
+
+cmake -G "Ninja Multi-Config" -C $TEMP_FOLDER/external/helper.cmake -S $SRC_DIR -B $BUILD_DIR
+if [ $? -ne 0 ]
+then
+    echo "Error generating cmake project"
+    exit 1
+fi
+
 cmake --build $BUILD_DIR --config Release --target clean
+if [ $? -ne 0 ]
+then
+    echo "Error cleaning project"
+    exit 1
+fi
+
 cmake --build $BUILD_DIR --config Release
+if [ $? -ne 0 ]
+then
+    echo "Error building project"
+    exit 1
+fi
 
 mkdir -p $TMP_RELEASE_DIR
 mv $BUILD_DIR/layers/Release/* $TMP_RELEASE_DIR
