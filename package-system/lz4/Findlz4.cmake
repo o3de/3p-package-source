21063635c1c3aa4f47a517a9e673368d9665100b
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2023-03-21 12:31:00 -0500

    Adding Ubuntu 20.04 qemu and docker support for building lz4
    Updating the lz4 library to 1.9.4 to address the security vulnerability found in version 1.9.3
    
    Added ArchLinux qemu and docker support for building lz4 as well
    
    resolves #9007
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- package-system/lz4/Findlz4.cmake
+++ package-system/lz4/Findlz4.cmake
@@ -1,7 +1,7 @@
 #
 # Copyright (c) Contributors to the Open 3D Engine Project.
 # For complete copyright and license terms please see the LICENSE at the root of this distribution.
-# 
+#
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 #
 #
@@ -13,21 +13,23 @@ if (TARGET ${TARGET_WITH_NAMESPACE})
     return()
 endif()
 
-set(${LIB_NAME}_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/${LIB_NAME}/${LIB_NAME}/include)
-set(${LIB_NAME}_LIBS_DIR ${CMAKE_CURRENT_LIST_DIR}/${LIB_NAME}/${LIB_NAME}/lib)
+set(${LIB_NAME}_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/${LIB_NAME}/include)
+set(${LIB_NAME}_LIBS_DIR ${CMAKE_CURRENT_LIST_DIR}/${LIB_NAME}/lib)
 set(${LIB_NAME}_LIBRARY_DEBUG   ${${LIB_NAME}_LIBS_DIR}/../debug/lib/${CMAKE_STATIC_LIBRARY_PREFIX}lz4d${CMAKE_STATIC_LIBRARY_SUFFIX})
 set(${LIB_NAME}_LIBRARY_RELEASE ${${LIB_NAME}_LIBS_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}lz4${CMAKE_STATIC_LIBRARY_SUFFIX})
 
-set(${LIB_NAME}_LIBRARY
-    "$<$<CONFIG:profile>:${${LIB_NAME}_LIBRARY_RELEASE}>"
-    "$<$<CONFIG:release>:${${LIB_NAME}_LIBRARY_RELEASE}>"
-    "$<$<CONFIG:debug>:${${LIB_NAME}_LIBRARY_DEBUG}>")
 
-add_library(${TARGET_WITH_NAMESPACE} INTERFACE IMPORTED GLOBAL)
+add_library(${TARGET_WITH_NAMESPACE} STATIC IMPORTED GLOBAL)
 ly_target_include_system_directories(
     TARGET ${TARGET_WITH_NAMESPACE} INTERFACE ${${LIB_NAME}_INCLUDE_DIR})
-target_link_libraries(
-    ${TARGET_WITH_NAMESPACE}
-    INTERFACE ${${LIB_NAME}_LIBRARY})
+
+set_target_properties(${TARGET_WITH_NAMESPACE} PROPERTIES
+    IMPORTED_LOCATION ${${LIB_NAME}_LIBRARY_RELEASE}
+)
+
+if (${PAL_PLATFORM_NAME} STREQUAL "Windows")
+    # IMPORTED_LOCATION_DEBUG property could be set here if it is desired that Windows
+    # uses a debug version of lz4
+endif()
 
 set(${LIB_NAME}_FOUND True)
