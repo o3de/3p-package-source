181ef4f4c686a635467eaaa9c7ce291e5fc5f1aa
Gene Walters <genewalt@amazon.com>
2024-09-29 14:23:10 -0700

    Assimp build script updates. Installer expects the build directly in the src folder
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- package-system/assimp/build_assimp_unix_like.sh
+++ package-system/assimp/build_assimp_unix_like.sh
@@ -7,7 +7,8 @@
 #
 #
 
-cmake_base_command="cmake -S temp/src -B temp/build temp/src/CMakeLists.txt -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=\"$DOWNLOADED_PACKAGE_FOLDERS\" -DASSIMP_BUILD_ZLIB=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_NO_EXPORT=ON -DASSIMP_HUNTER_ENABLED=OFF -DASSIMP_BUILD_USD_IMPORTER=ON"
+# -Wno-error, turning off warnings as errors because Assimp uses TinyUSDZ for USD support which has compiler warnings.
+cmake_base_command="cmake -S temp/src -B temp/src temp/src/CMakeLists.txt -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=\"$DOWNLOADED_PACKAGE_FOLDERS\" -DASSIMP_BUILD_ZLIB=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=ON -DASSIMP_HUNTER_ENABLED=OFF -DASSIMP_BUILD_USD_IMPORTER=ON -DASSIMP_WARNINGS_AS_ERRORS=OFF -DASSIMP_BUILD_TESTS=ON -DCMAKE_CXX_FLAGS=\"-Wno-unused-const-variable -Wno-error\""
 
 # On Mac, load the toolchain file to make sure
 # the build matches compatibility with other Mac libraries
@@ -24,16 +25,13 @@ echo "Running first cmake command:"
 echo "$cmake_no_shared_libs"
 
 eval "$cmake_no_shared_libs" || exit 1
-cmake --build temp/build --config release || exit 1
+cmake --build temp/src --config release || exit 1
 
 echo "Running second cmake command:"
 echo "$cmake_shared_libs"
 
 eval "$cmake_shared_libs" || exit 1
-cmake --build temp/build --config release || exit 1
-
-mkdir temp/build/port/
-cp -R temp/src/port/PyAssimp temp/build/port/
+cmake --build temp/src --config release || exit 1
 
 if [ "$(uname)" = "Darwin" ];
 then
