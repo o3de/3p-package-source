11c082c94a4e59f185d3faa11143afa2af047394
Gene Walters <32776221+AMZN-Gene@users.noreply.github.com>
2024-10-02 10:07:21 -0700

    AssImp 5.4.3: Universal Scene Description (USD) (#264)
    Migrating to AssImp 5.4.3 
    - Enabling USD (AssImp's USD imports TinyUSDZ which compiles with
    warnings, so had to turn off compiler warnings as errors)
     - Disable building tools as they aren't required (assimp.exe)
    - Making sure installation scripts works. Build scripts and installer
    scripts were looking in different folders (src vs build folder)
    - Long term it would be better update scripts built into a separate
    "build" folder.
    
    
    ![image](https://github.com/user-attachments/assets/81c6a103-b7c5-455e-9362-a1f2b91a464a)
    
    ---------
    
    Signed-off-by: AMZN-Gene <genewalt@amazon.com>
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- package-system/assimp/build_assimp_unix_like.sh
+++ package-system/assimp/build_assimp_unix_like.sh
@@ -7,7 +7,8 @@
 #
 #
 
-cmake_base_command="cmake -S temp/src -B temp/src  -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=\"$DOWNLOADED_PACKAGE_FOLDERS\" -DASSIMP_BUILD_ZLIB=OFF"
+# -Wno-error, turning off warnings as errors because Assimp uses TinyUSDZ for USD support which has compiler warnings.
+cmake_base_command="cmake -S temp/src -B temp/src temp/src/CMakeLists.txt -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=\"$DOWNLOADED_PACKAGE_FOLDERS\" -DASSIMP_BUILD_ZLIB=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_HUNTER_ENABLED=OFF -DASSIMP_BUILD_USD_IMPORTER=ON -DASSIMP_WARNINGS_AS_ERRORS=OFF -DASSIMP_BUILD_TESTS=ON -DCMAKE_CXX_FLAGS=\"-Wno-unused-const-variable -Wno-error\""
 
 # On Mac, load the toolchain file to make sure
 # the build matches compatibility with other Mac libraries
@@ -17,27 +18,17 @@ then
     cmake_base_command+=" -DCMAKE_TOOLCHAIN_FILE=$PWD/../../Scripts/cmake/Platform/Mac/Toolchain_mac.cmake"
 fi
 
-cmake_no_shared_libs="$cmake_base_command -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=ON"
-cmake_shared_libs="$cmake_base_command -DBUILD_SHARED_LIBS=ON -DASSIMP_BUILD_ASSIMP_TOOLS=ON"
+cmake_no_shared_libs="$cmake_base_command -DBUILD_SHARED_LIBS=OFF"
+cmake_shared_libs="$cmake_base_command -DBUILD_SHARED_LIBS=ON"
 
 echo "Running first cmake command:"
 echo "$cmake_no_shared_libs"
 
-eval "$cmake_no_shared_libs temp/src/CMakeLists.txt" || exit 1
-cmake --build temp/build --config release || exit 1
+eval "$cmake_no_shared_libs" || exit 1
+cmake --build temp/src --config release || exit 1
 
 echo "Running second cmake command:"
 echo "$cmake_shared_libs"
 
-eval "$cmake_shared_libs temp/src/CMakeLists.txt" || exit 1
-cmake --build temp/build --config release || exit 1
-
-mkdir temp/build/port/
-cp -R temp/src/port/PyAssimp temp/build/port/
-
-if [ "$(uname)" = "Darwin" ];
-then
-    # Printing the minimum OS version here can save some time debugging.
-    echo "Min OS version:"
-    otool -l temp/src/bin/assimp | grep -i minos
-fi
+eval "$cmake_shared_libs" || exit 1
+cmake --build temp/src --config release || exit 1
--- package-system/assimp/build_assimp_windows.cmd
+++ package-system/assimp/build_assimp_windows.cmd
@@ -12,22 +12,30 @@
 @rem # cmake expects fowardslashes:
 set "DOWNLOADED_PACKAGE_FOLDERS=%DOWNLOADED_PACKAGE_FOLDERS:\=/%"
 
-cmake -S temp/src ^
+@rem # /w compiler option. Assimp USD is implemented using TinyUSDZ which, unfortunately, contains compiler warnings
+cmake -S temp/src -G "Visual Studio 17" ^
     -DBUILD_SHARED_LIBS=OFF ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_MODULE_PATH="%DOWNLOADED_PACKAGE_FOLDERS%" ^
     -DASSIMP_BUILD_ZLIB=OFF ^
-    -DASSIMP_BUILD_ASSIMP_TOOLS=ON ^
+    -DASSIMP_BUILD_ASSIMP_TOOLS=OFF ^
+    -DASSIMP_BUILD_USD_IMPORTER=ON ^
+    -DASSIMP_WARNINGS_AS_ERRORS=OFF ^
+    -DCMAKE_CXX_FLAGS="/EHsc /w" ^
     temp/src/CMakeLists.txt || exit /b 1
 cmake --build temp/src --config release || exit /b 1
 cmake --build temp/src --config debug || exit /b 1
 
-cmake -S temp/src ^
+@rem # /w compiler option. Assimp USD is implemented using TinyUSDZ which, unfortunately, contains compiler warnings
+cmake -S temp/src -G "Visual Studio 17" ^
     -DBUILD_SHARED_LIBS=ON ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_MODULE_PATH="%DOWNLOADED_PACKAGE_FOLDERS%" ^
     -DASSIMP_BUILD_ZLIB=OFF ^
-    -DASSIMP_BUILD_ASSIMP_TOOLS=ON ^
+    -DASSIMP_BUILD_ASSIMP_TOOLS=OFF ^
+    -DASSIMP_BUILD_USD_IMPORTER=ON ^
+    -DASSIMP_WARNINGS_AS_ERRORS=OFF ^
+    -DCMAKE_CXX_FLAGS="/EHsc /w" ^
     temp/src/CMakeLists.txt || exit /b 1
 cmake --build temp/src --config release || exit /b 1
 cmake --build temp/src --config debug || exit /b 1
