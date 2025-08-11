37d1bc821f164a2d95d1b09e7b662f5d6b26e2bb
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2025-08-11 08:44:05 -0700

    Remove AWS cloudfront reference in the package downloader URL (#291)
    Remove AWS cloudfront reference in the package downloader URL. There
    were multiple ones, and the second one is the valid one.
    
    ---------
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Scripts/extras/package_downloader.py
+++ Scripts/extras/package_downloader.py
@@ -18,9 +18,8 @@ import tarfile
 import sys
 from urllib.parse import _splithost
 
-
 # used if LY_PACKAGE_SERVER_URLS is not set.
-DEFAULT_LY_PACKAGE_SERVER_URLS = "https://d2c171ws20a1rv.cloudfront.net;https://d3t6xeg4fgfoum.cloudfront.net"
+DEFAULT_LY_PACKAGE_SERVER_URLS = "https://d1gg6ket0m44ly.cloudfront.net;https://d3t6xeg4fgfoum.cloudfront.net"
 
 possible_download_errors = (ssl.SSLError, URLError, OSError)
 
