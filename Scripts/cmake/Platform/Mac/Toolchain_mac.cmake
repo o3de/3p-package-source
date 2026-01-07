486a365b53b7fff98d48533812e27eeb0df9be35
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2026-01-06 21:54:36 -0800

    Mac ARM64 3P support : Initial setup for Mac ARM64 (#297)
    * Create new package_build_list* file for darwin-arm64
    * Update Mac Toolchain to deployment target of 15
    * Update build-package.yaml to use os 'macos-15'
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Scripts/cmake/Platform/Mac/Toolchain_mac.cmake
+++ Scripts/cmake/Platform/Mac/Toolchain_mac.cmake
@@ -11,7 +11,7 @@
 # in all 3p packages so that they don't have to seperately define this for each one.
 
 set(CMAKE_SYSTEM_NAME Darwin)
-set(CMAKE_OSX_DEPLOYMENT_TARGET "11.0" CACHE STRING "The minimum OSX Version to support" FORCE)
+set(CMAKE_OSX_DEPLOYMENT_TARGET "15.0" CACHE STRING "The minimum OSX Version to support" FORCE)
 set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
 
 # If we need to compile for Mac M1, set CMAKE_APPLE_SILICON_PROCESSOR to arm64 on the command line.
