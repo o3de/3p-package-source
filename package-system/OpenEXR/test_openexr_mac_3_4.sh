c89d2e093f5ae9e54351066c9f13dfb2d673c376
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2026-01-17 23:52:16 -0800

    Mac ARM64 3P support: Add definition for OpenEXR-3.4.4-rev1-mac-arm64 (#330)
    * Add new entry in package-system/OpenEXR/build_config.json for
    `Mac-arm64`
    * Updated version of OpenEXR to 3.4.4 for Mac-arm64
    * Added `OpenEXR-3.4.4-rev1-mac-arm64` into package_build_list_host_darwin-arm64.json`
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

---
+++ package-system/OpenEXR/test_openexr_mac_3_4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+# 
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+
+rm -rf temp/build_test
+mkdir temp/build_test
+
+cmake -S test -B temp/build_test -G Xcode \
+    -DCMAKE_TOOLCHAIN_FILE=../../../../Scripts/cmake/Platform/Mac/Toolchain_mac.cmake \
+    -DCMAKE_MODULE_PATH="$DOWNLOADED_PACKAGE_FOLDERS;$PACKAGE_ROOT" \
+    -DOPENEXR_TEST_VER_3_4=TRUE || exit 1
+
+cmake --build temp/build_test --parallel --config Release || exit 1
+temp/build_test/Release/test_openexr.app/Contents/MacOS/test_openexr || exit 1
+
+cmake --build temp/build_test --parallel --config Debug || exit 1
+temp/build_test/Debug/test_openexr.app/Contents/MacOS/test_openexr || exit 1
+
+exit 0
