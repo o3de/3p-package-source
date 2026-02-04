c52776a35f1b84f5ed3a5c9df959f831cdf548a6
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2026-02-03 23:23:05 -0800

    Update build script to configure and build zstd
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- package-system/zstd/build_zstd_mac.sh
+++ package-system/zstd/build_zstd_mac.sh
@@ -5,10 +5,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
-echo "Configuring cityhash"
+echo "Configuring zstd"
 cd src
-
-# ZSTD_BUILD_STATIC
 
 SRC_DIR=$TEMP_FOLDER/src
 BLD_DIR=$TEMP_FOLDER/build
@@ -17,14 +15,17 @@ INSTALL_DIR=$TEMP_FOLDER/install
 cmake -S $SRC_DIR/build/cmake -B $BLD_DIR -DZSTD_BUILD_STATIC=ON -DZSTD_BUILD_PROGRAMS=OFF -DZSTD_BUILD_TESTS=OFF -DZSTD_BUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
 if [ $? -ne 0 ]
 then
-    echo "Failed configuring cityhash"
+    echo "Failed configuring zstd"
     exit 1
 fi
 
 cmake --build ${BLD_DIR} --config Release --target install 
 if [ $? -ne 0 ]
 then
-    echo "Failed building cityhash"
+    echo "Failed building zstd"
     exit 1
 fi
+
+echo "Finished building zstd"
+
  exit 0
