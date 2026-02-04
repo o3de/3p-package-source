ef3fae895e7be2a331a2b27311b81d71934771fd
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2026-02-03 21:56:37 -0800

    Mac ARM64 3P support: Add definition for cityhash-1.1-rev1-mac-arm64
    * Created new build_cityhash_mac.sh
    * Created new install_cityhash_mac.sh
    * Add new entry in build_config.json for `Mac-arm64` (using alias '@Mac')
    * Added `cityhash-1.1-rev1-mac-arm64` into package_build_list_host_darwin-arm64.json`
    * Added Findcityhash.cmake.mac
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

---
+++ package-system/zstd/build_zstd_mac.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+echo "Configuring cityhash"
+cd src
+
+# ZSTD_BUILD_STATIC
+
+SRC_DIR=$TEMP_FOLDER/src
+BLD_DIR=$TEMP_FOLDER/build
+INSTALL_DIR=$TEMP_FOLDER/install
+
+cmake -S $SRC_DIR/build/cmake -B $BLD_DIR -DZSTD_BUILD_STATIC=ON -DZSTD_BUILD_PROGRAMS=OFF -DZSTD_BUILD_TESTS=OFF -DZSTD_BUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+if [ $? -ne 0 ]
+then
+    echo "Failed configuring cityhash"
+    exit 1
+fi
+
+cmake --build ${BLD_DIR} --config Release --target install 
+if [ $? -ne 0 ]
+then
+    echo "Failed building cityhash"
+    exit 1
+fi
+ exit 0
