02f27c5150a35ebd0117543e133ab30c07db3b50
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2026-02-12 23:05:17 -0800

    Fix wrapper package scripts and add submodule path detection for windows. Update README.md for package instructions
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- package.bat
+++ package.bat
@@ -1,7 +1,24 @@
 @echo off
-rem Launch build_package.py in this directory with all forwarded arguments
+REM 
+REM  Copyright (c) Contributors to the Open 3D Engine Project. For complete copyright and license terms please see the LICENSE at the root of this distribution.
+REM 
+REM  SPDX-License-Identifier: Apache-2.0 OR MIT
+REM 
+REM 
+
 setlocal
-set "SCRIPT=%~dp0package.py"
+
+set BASEPATH=%~dp0
+set SCRIPT=%BASEPATH%\package.py
+set PACKAGE_SCRIPT_REQUIREMENTS=%BASEPATH%\Scripts\packaging\requirements.txt
+
+REM Check for the packaging script and its requirements
+if not exist "%PACKAGE_SCRIPT_REQUIREMENTS%" (
+  echo ERROR: One or more git submodules are not initialized.
+  echo Run: git submodule update --init --recursive
+  exit /b 1
+)
+
 where py >nul 2>&1
 if %ERRORLEVEL%==0 (
   py "%SCRIPT%" %*
--- package.sh
+++ package.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (c) Contributors to the Open 3D Engine Project. For complete copyright and license terms please see the LICENSE at the root of this distribution.
+# 
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
 # Launch package.py in this directory with all forwarded arguments
 
 BASE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
