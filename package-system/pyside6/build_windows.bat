3e6d5c711ada9630f6ecfed56127bd5f06f00fd1
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2026-03-03 22:22:07 -0800

    Additional Fixes
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- package-system/pyside6/build_windows.bat
+++ package-system/pyside6/build_windows.bat
@@ -48,15 +48,25 @@ cd %TEMP_FOLDER%\src
 echo "Installing build dependencies"
 %TEMP_FOLDER%\python-3.10.13-rev1-windows\python\python.exe -m pip install -r %TEMP_FOLDER%\src\requirements.txt
 
-set LLVM_INSTALL_DIR=%TEMP_FOLDER%\libclang-release_130-based-windows-vs2019_64\libclang
+set LLVM_INSTALL_DIR=%TEMP_FOLDER%\libclang-release_140-based-windows-vs2019_64\libclang
 set PATH=%LLVM_INSTALL_DIR%\bin;%PATH%
 
 echo LLVM_INSTALL_DIR=%LLVM_INSTALL_DIR%
 echo PATH=%PATH%
 
 echo Building Pyside6
-%TEMP_FOLDER%\python-3.10.13-rev1-windows\python\python.exe setup.py build --qtpaths=%TEMP_FOLDER%\qt-6.10.2-rev1-windows\qt\bin\qtpaths6.exe --build-tests --ignore-git --parallel=8 --build-type=all --debug --limited-api=no --skip-modules=Qml,Quick,QuickWidgets,Positioning,Location,RemoteObjects,Scxml,TextToSpeech,3DCore,3DRender,3DInput,3DLogic,3DAnimation,3DExtras,Multimedia,MultimediaWidgets,AxContainer
+%TEMP_FOLDER%\python-3.10.13-rev1-windows\python\python.exe setup.py build ^
+    --qtpaths=%TEMP_FOLDER%\qt-6.10.2-rev4-windows\qt\bin\qtpaths6.exe ^
+    --ignore-git ^
+    --parallel=8 ^
+    --build-type=all ^
+    --skip-docs ^
+    --log-level=verbose ^
+    --limited-api=no ^
+    --skip-modules=Quick,MultimediaWidgets,Pdf,PdfWidgets,Positioning,Location,NetworkAuth,Nfc,WebEngineQuick,Multimedia,QuickControls2,QuickTest,QuickWidgets,UiToolsPrivate,RemoteObjects
 
 REM  --openssl=c:\path\to\openssl\bin --build-tests --ignore-git --parallel=8
 REM %TEMP_FOLDER%\qt-6.10.2-rev1-windows\qt\bin\qtpaths.exe --qtpaths-target=host --qtpaths-query=libexec > qt_host_libexec_path.txt
 exit /B 1
+
+  
