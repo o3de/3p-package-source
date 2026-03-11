b108be9b16fb7d6aa3faa7550b227b459f9425c4
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2026-03-10 20:58:05 -0700

    Update to use venv
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- package-system/pyside6/build_windows.bat
+++ package-system/pyside6/build_windows.bat
@@ -17,7 +17,6 @@ echo.
 
 REM Set these before running the script
 if not defined VCVARS_PATH set VCVARS_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-if not defined QTARRAY set QTARRAY=qtbase,qtimageformats,qtsvg,qttranslations
 
 REM TEMP_FOLDER and TARGET_INSTALL_ROOT get set from the pull_and_build_from_git.py script
 set CHECKS_FAILED=0
@@ -33,40 +32,45 @@ if %CHECKS_FAILED%==1 goto FAILURE
 echo Setting up VS2022
 call %VCVARS_PATH% amd64
 
-REM For OpenSSL support
-set OPENSSL_ROOT=%TEMP_FOLDER%\OpenSSL-1.1.1o-rev1-windows\OpenSSL
-set OPENSSL_INCLUDE=%OPENSSL_ROOT%\include
-set OPENSSL_LIB_DEBUG=%OPENSSL_ROOT%\debug\lib
-set OPENSSL_LIB_RELEASE=%OPENSSL_ROOT%\lib
-set INCLUDE=%OPENSSL_INCLUDE%;%INCLUDE%
-set LIB=%OPENSSL_LIB_DEBUG%;%OPENSSL_LIB_RELEASE%;%LIB%
-
-
 echo "cd src"
+
+echo Setup Python Virtual ENV
+
+set LOCAL_PYTHON_BIN=%TEMP_FOLDER%\python-3.10.13-rev1-windows\python\python.exe
+
+echo Created Python Virtual ENV
+
+cd %TEMP_FOLDER%
+%LOCAL_PYTHON_BIN% -m venv testenv
+call testenv\Scripts\activate
+call testenv\Scripts\pip.exe install -r %TEMP_FOLDER%\src\requirements.txt
+if %ERRORLEVEL% NEQ 0 (
+    echo "Failed to create venv"
+    exit /B 1
+)
+
 cd %TEMP_FOLDER%\src
 
 echo "Installing build dependencies"
-%TEMP_FOLDER%\python-3.10.13-rev1-windows\python\python.exe -m pip install -r %TEMP_FOLDER%\src\requirements.txt
+
+ECHO Building Pyside2 (Release)
 
 set LLVM_INSTALL_DIR=%TEMP_FOLDER%\libclang-release_140-based-windows-vs2019_64\libclang
 set PATH=%LLVM_INSTALL_DIR%\bin;%PATH%
 
-echo LLVM_INSTALL_DIR=%LLVM_INSTALL_DIR%
-echo PATH=%PATH%
-
-echo Building Pyside6
-%TEMP_FOLDER%\python-3.10.13-rev1-windows\python\python.exe setup.py build ^
+call %TEMP_FOLDER%\testenv\Scripts\python.exe setup.py install ^
     --qtpaths=%TEMP_FOLDER%\qt-6.10.2-rev4-windows\qt\bin\qtpaths6.exe ^
     --ignore-git ^
     --parallel=8 ^
     --build-type=all ^
     --skip-docs ^
     --log-level=verbose ^
-    --limited-api=no ^
-    --skip-modules=Quick,MultimediaWidgets,Pdf,PdfWidgets,Positioning,Location,NetworkAuth,Nfc,WebEngineQuick,Multimedia,QuickControls2,QuickTest,QuickWidgets,UiToolsPrivate,RemoteObjects
-
-REM  --openssl=c:\path\to\openssl\bin --build-tests --ignore-git --parallel=8
-REM %TEMP_FOLDER%\qt-6.10.2-rev1-windows\qt\bin\qtpaths.exe --qtpaths-target=host --qtpaths-query=libexec > qt_host_libexec_path.txt
-exit /B 1
+    --limited-api=yes ^
+    --skip-modules=Quick,MultimediaWidgets,Pdf,PdfWidgets,Positioning,Location,NetworkAuth,Nfc,WebEngineQuick,Multimedia,QuickControls2,QuickTest,QuickWidgets,UiToolsPrivate,RemoteObjects,Positioning,Scxml,TextToSpeech,3DCore,3DRender,3DInput,3DLogic,3DAnimation,3DExtras,AxContainer
+if %ERRORLEVEL% NEQ 0 (
+    echo "Failed to build pyside2 (release)"
+    exit /B 1
+)
+exit /B 0
 
   
